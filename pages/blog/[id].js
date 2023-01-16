7c4424edb362429bd3492ffc71fc4c965eac218a
thr.hryk <thr.hryk@gmail.com>
2023-01-16 18:28:59 +0900

    fix post css

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,12 +1,12 @@
 // pages/blog/[id].js
 import { client } from "../../libs/client";
-import styles from '../../styles/Home.module.scss';
+// import styles from '../../styles/Home.module.scss';
 
 export default function BlogId({ blog }) {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>{blog.title}</h1>
-      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+    <main>
+      <h1>{blog.title}</h1>
+      <p>{blog.publishedAt}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
